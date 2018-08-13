5d920f66a126e32512157fcac1d114affe1b0f36
qazx7412 <git@newgenerations.im>
2018-08-13 12:14:37 +0900

    TCP改良

--- src/echo.cr
+++ src/echo.cr
@@ -2,11 +2,15 @@ require "./echo/*"
 require "socket"
 
 module Echo
+  # TCP鯖
   server = TCPServer.new(2000)
   counter : Int64 = 0
   loop do
     server.accept do |socket|
       p socket
+      while buffer = socket.gets
+        puts buffer
+      end
       socket.puts "Hello #{Time.now} counter:#{counter}"
       socket.close
       counter += 1
