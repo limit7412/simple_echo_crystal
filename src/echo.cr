24ce0fc74193023246d3190c3cfe10a3252aaee5
qazx7412 <git@newgenerations.im>
2018-08-13 16:02:50 +0900

    標準のやつ

--- src/echo.cr
+++ src/echo.cr
@@ -1,6 +1,6 @@
 require "./echo/*"
-require "socket"
-
+# require "socket"
+require "http/server"
 
 module Echo
   # # TCP鯖
@@ -18,23 +18,32 @@ module Echo
   #   end
   # end
 
-  # シンプルHTTP鯖
-  server = TCPServer.new(2000)
-  loop do
-    server.accept do |socket|
-      p socket
-      headers : Array(String) = Array(String).new
-      while header = socket.gets
-        break if header.chomp.empty?
-        headers << header.chomp
-      end
-      p headers
+  # # シンプルHTTP鯖
+  # server = TCPServer.new(2000)
+  # loop do
+  #   server.accept do |socket|
+  #     p socket
+  #     headers : Array(String) = Array(String).new
+  #     while header = socket.gets
+  #       break if header.chomp.empty?
+  #       headers << header.chomp
+  #     end
+  #     p headers
 
-      socket.puts "HTTP/1.0 200 OK"
-      socket.puts "Content-Type: text/plain"
-      socket.puts
-      socket.puts "message body"
-      socket.close
-    end
+  #     socket.puts "HTTP/1.0 200 OK"
+  #     socket.puts "Content-Type: text/plain"
+  #     socket.puts
+  #     socket.puts "message body"
+  #     socket.close
+  #   end
+  # end
+
+  # HTTP
+  server = HTTP::Server.new do |context|
+    context.response.content_type = "text/plain"
+    context.response.print "Hello world!"
   end
+  server.bind_tcp 2000
+  puts "Listening"
+  server.listen
 end
